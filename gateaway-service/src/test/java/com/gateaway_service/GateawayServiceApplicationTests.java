051e038e1883e6ebe321a63a10857d5d8d684b69
joserivera <jrivera46@cuc.edu.co>
2025-02-28 11:40:50 -0500

    gateway

---
+++ gateaway-service/src/test/java/com/gateaway_service/GateawayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gateaway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GateawayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
