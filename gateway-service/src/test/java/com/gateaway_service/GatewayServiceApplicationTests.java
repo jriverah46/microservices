31e95464e0fbe608660e8414367af8df56baa55d
joserivera <jrivera46@cuc.edu.co>
2025-02-28 12:21:24 -0500

    gateway

--- gateway-service/src/test/java/com/gateaway_service/GatewayServiceApplicationTests.java
+++ gateway-service/src/test/java/com/gateaway_service/GatewayServiceApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GateawayServiceApplicationTests {
+class GatewayServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
